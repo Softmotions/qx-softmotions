0f7075a27245081eba514feecadc289fbd810d8b
adam <anton@adamansky.com>
2011-04-05 18:06:16 +0700

    nkserver

--- node/qxoo/class/sm/nsrv/VHostEngine.js
+++ node/qxoo/class/sm/nsrv/VHostEngine.js
@@ -203,7 +203,7 @@ qx.Class.define("sm.nsrv.VHostEngine", {
         },
 
         getAssembly : function(name) {
-            return this.__assembly[name];
+            return this.__assembly[name] ? this.__assembly[name] : {};
         },
 
         getAssemblyMap : function() {
