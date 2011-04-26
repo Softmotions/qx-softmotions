dd55caf1b4d03b031323994728e3738e158b681c
IPhoenix <iphoenix@iphoenix.(none)>
2011-04-26 15:14:02 +0700

    тесты на logout

--- node/test/nksrv/test-nsrv-security.js
+++ node/test/nksrv/test-nsrv-security.js
@@ -328,6 +328,111 @@ buildBasicRolesTests("Test access resource: required roles ['a', 'b'], user role
                          { auth: "dGVzdDQ6dGVzdA==", access: true, roles: "['b', 'c', 'a']" }
                      ]);
 
+module.exports["Test logout after auth (Basic)"] = function(test) {
+    var client = http.createClient(port, "127.0.0.1");
+    var req1 = client.request("GET", "/test/secured", { "host": "127.0.0.1", "Authorization": "Basic dGVzdDp0ZXN0" });
+    req1.on("response", function (resp1) {
+        resp1.setEncoding("utf8");
+        test.equal(resp1.statusCode, 200);
+        var cookie = "";
+        test.ok(resp1.headers["set-cookie"]);
+        if (resp1.headers["set-cookie"]) {
+            var cresp = resp1.headers["set-cookie"];
+            cresp.forEach(function(item) {
+                cookie += item.split(';')[0] + "; "
+            });
+        }
+        test.equal(resp1.headers["content-type"], "text/plain");
+        resp1.on("data", function (body) {
+            test.ok(body);
+            test.ok(body.indexOf("content") >= 0)
+        });
+        resp1.on("end", function () {
+            var req2 = client.request("GET", "/test/secured", { "host": "127.0.0.1", "cookie": cookie });
+            req2.on("response", function (resp2) {
+                resp2.setEncoding("utf8");
+                test.equal(resp2.statusCode, 200);
+                test.equal(resp2.headers["content-type"], "text/plain");
+                resp2.on("data", function (body) {
+                    test.ok(body);
+                    test.ok(body.indexOf("content") >= 0)
+                });
+                resp2.on("end", function () {
+                    var req3 = client.request("GET", "/test/logout", { "host": "127.0.0.1", "cookie": cookie });
+                    req3.on("response", function (resp3) {
+                        resp3.setEncoding("utf8");
+                        test.equal(resp3.statusCode, 200);
+                        test.equal(resp3.headers["content-type"], "text/plain");
+                        resp3.on("data", function (body) {
+                            test.ok(body);
+                            test.ok(body.indexOf("content") >= 0)
+                        });
+                        resp3.on("end", function () {
+                            var req4 = client.request("GET", "/test/secured", { "host": "127.0.0.1", "cookie": cookie });
+                            req4.on("response", function (resp4) {
+                                resp4.setEncoding("utf8");
+                                test.equal(resp4.statusCode, 401);
+                                test.equal(resp4.headers["www-authenticate"], "Basic realm=\"NKServerTest\"");
+                                resp4.on("end", function () {
+                                    test.done();
+                                });
+                            });
+                            req4.end();
+                        });
+                    });
+                    req3.end();
+                });
+            });
+            req2.end();
+        });
+    });
+    req1.end();
+};
+
+module.exports["Test logout without auth (Basic)"] = function(test) {
+    var client = http.createClient(port, "127.0.0.1");
+    var req1 = client.request("GET", "/test/secured", { "host": "127.0.0.1" });
+    req1.on("response", function (resp1) {
+        resp1.setEncoding("utf8");
+        test.equal(resp1.statusCode, 401);
+        test.equal(resp1.headers["www-authenticate"], "Basic realm=\"NKServerTest\"");
+        var cookie = "";
+        test.ok(resp1.headers["set-cookie"]);
+        if (resp1.headers["set-cookie"]) {
+            var cresp = resp1.headers["set-cookie"];
+            cresp.forEach(function(item) {
+                cookie += item.split(';')[0] + "; "
+            });
+        }
+        resp1.on("end", function () {
+            var req2 = client.request("GET", "/test/logout", { "host": "127.0.0.1", "cookie": cookie });
+            req2.on("response", function (resp2) {
+                resp2.setEncoding("utf8");
+                test.equal(resp2.statusCode, 200);
+                test.equal(resp2.headers["content-type"], "text/plain");
+                resp2.on("data", function (body) {
+                    test.ok(body);
+                    test.ok(body.indexOf("content") >= 0)
+                });
+                resp2.on("end", function () {
+                    var req3 = client.request("GET", "/test/secured", { "host": "127.0.0.1", "cookie": cookie });
+                    req3.on("response", function (resp3) {
+                        resp3.setEncoding("utf8");
+                        test.equal(resp3.statusCode, 401);
+                        test.equal(resp3.headers["www-authenticate"], "Basic realm=\"NKServerTest\"");
+                        resp3.on("end", function () {
+                            test.done();
+                        });
+                    });
+                    req3.end();
+                });
+            });
+            req2.end();
+        });
+    });
+    req1.end();
+};
+
 module.exports["Tes shutdown server (Basic)"] = function(test) {
     test.ok(nserver);
     nserver.shutdown();
@@ -609,13 +714,21 @@ module.exports["Test secured resource: double request with auth, but without coo
     req1.end();
 };
 
-
 module.exports["Test secured resource: double request with auth and with cookie (Digest)"] = function(test) {
     var client = http.createClient(port, "127.0.0.1");
     var req1 = client.request("GET", "/test/secured", { "host": "127.0.0.1" });
     req1.on("response", function (resp1) {
         resp1.setEncoding("utf8");
         test.equal(resp1.statusCode, 401);
+        test.ok(resp1.headers["set-cookie"]);
+        var cookie = "";
+        test.ok(resp1.headers["set-cookie"]);
+        if (resp1.headers["set-cookie"]) {
+            var cresp = resp1.headers["set-cookie"];
+            cresp.forEach(function(item) {
+                cookie += item.split(';')[0] + "; "
+            });
+        }
         test.ok(resp1.headers["www-authenticate"]);
         var match = resp1.headers["www-authenticate"].match(/^Digest\s+(.*)/);
         test.ok(match);
@@ -629,20 +742,12 @@ module.exports["Test secured resource: double request with auth and with cookie 
 
                 var auth = generateDigestAuth(authreq, "/test/secured", "test", "test");
 
-                var req2 = client.request("GET", "/test/secured", {"host": "127.0.0.1", "Authorization": auth});
+                var req2 = client.request("GET", "/test/secured", {"host": "127.0.0.1", "cookie": cookie, "Authorization": auth});
                 req2.on("response", function (resp2) {
                     resp2.setEncoding("utf8");
                     test.equal(resp2.statusCode, 200);
                     test.equal(resp2.headers["content-type"], "text/plain");
                     test.ok(resp2.headers["set-cookie"]);
-                    var cookie = "";
-                    test.ok(resp2.headers["set-cookie"]);
-                    if (resp2.headers["set-cookie"]) {
-                        var cresp = resp2.headers["set-cookie"];
-                        cresp.forEach(function(item) {
-                            cookie += item.split(';')[0] + "; "
-                        });
-                    }
                     resp2.on("data", function (body) {
                         test.ok(body);
                         test.ok(body.indexOf("content") >= 0)
@@ -727,40 +832,160 @@ var buildDigestRolesTests = function(name, resource, tests) {
 
 
 buildDigestRolesTests("Test access resource: required roles ['a'], user roles:",
-                     "/test/roles/a",
-                     [
-                         { auth: {login: "test", password: "test"}, access: true, roles: "['a']" },
-                         { auth: {login: "test2", password: "test"}, access: false, roles: "['b']" },
-                         { auth: {login: "test3", password: "test"}, access: true, roles: "['c', 'a']" },
-                         { auth: {login: "test4", password: "test"}, access: true, roles: "['b', 'c', 'a']" }
-                     ]);
+                      "/test/roles/a",
+                      [
+                          { auth: {login: "test", password: "test"}, access: true, roles: "['a']" },
+                          { auth: {login: "test2", password: "test"}, access: false, roles: "['b']" },
+                          { auth: {login: "test3", password: "test"}, access: true, roles: "['c', 'a']" },
+                          { auth: {login: "test4", password: "test"}, access: true, roles: "['b', 'c', 'a']" }
+                      ]);
 
 buildDigestRolesTests("Test access resource: required roles ['b'], user roles:",
-                     "/test/roles/b",
-                     [
-                         { auth: {login: "test", password: "test"}, access: false, roles: "['a']" },
-                         { auth: {login: "test2", password: "test"}, access: true, roles: "['b']" },
-                         { auth: {login: "test3", password: "test"}, access: false, roles: "['c', 'a']" },
-                         { auth: {login: "test4", password: "test"}, access: true, roles: "['b', 'c', 'a']" }
-                     ]);
+                      "/test/roles/b",
+                      [
+                          { auth: {login: "test", password: "test"}, access: false, roles: "['a']" },
+                          { auth: {login: "test2", password: "test"}, access: true, roles: "['b']" },
+                          { auth: {login: "test3", password: "test"}, access: false, roles: "['c', 'a']" },
+                          { auth: {login: "test4", password: "test"}, access: true, roles: "['b', 'c', 'a']" }
+                      ]);
 
 buildDigestRolesTests("Test access resource: required roles ['c'], user roles:",
-                     "/test/roles/c",
-                     [
-                         { auth: {login: "test", password: "test"}, access: false, roles: "['a']" },
-                         { auth: {login: "test2", password: "test"}, access: false, roles: "['b']" },
-                         { auth: {login: "test3", password: "test"}, access: true, roles: "['c', 'a']" },
-                         { auth: {login: "test4", password: "test"}, access: true, roles: "['b', 'c', 'a']" }
-                     ]);
+                      "/test/roles/c",
+                      [
+                          { auth: {login: "test", password: "test"}, access: false, roles: "['a']" },
+                          { auth: {login: "test2", password: "test"}, access: false, roles: "['b']" },
+                          { auth: {login: "test3", password: "test"}, access: true, roles: "['c', 'a']" },
+                          { auth: {login: "test4", password: "test"}, access: true, roles: "['b', 'c', 'a']" }
+                      ]);
 
 buildDigestRolesTests("Test access resource: required roles ['a', 'b'], user roles:",
-                     "/test/roles/d",
-                     [
-                         { auth: {login: "test", password: "test"}, access: false, roles: "['a']" },
-                         { auth: {login: "test2", password: "test"}, access: false, roles: "['b']" },
-                         { auth: {login: "test3", password: "test"}, access: false, roles: "['c', 'a']" },
-                         { auth: {login: "test4", password: "test"}, access: true, roles: "['b', 'c', 'a']" }
-                     ]);
+                      "/test/roles/d",
+                      [
+                          { auth: {login: "test", password: "test"}, access: false, roles: "['a']" },
+                          { auth: {login: "test2", password: "test"}, access: false, roles: "['b']" },
+                          { auth: {login: "test3", password: "test"}, access: false, roles: "['c', 'a']" },
+                          { auth: {login: "test4", password: "test"}, access: true, roles: "['b', 'c', 'a']" }
+                      ]);
+
+module.exports["Test logout after auth (Digest)"] = function(test) {
+    var client = http.createClient(port, "127.0.0.1");
+    var req1 = client.request("GET", "/test/secured", { "host": "127.0.0.1" });
+    req1.on("response", function (resp1) {
+        resp1.setEncoding("utf8");
+        test.equal(resp1.statusCode, 401);
+        test.ok(resp1.headers["set-cookie"]);
+        var cookie = "";
+        test.ok(resp1.headers["set-cookie"]);
+        if (resp1.headers["set-cookie"]) {
+            var cresp = resp1.headers["set-cookie"];
+            cresp.forEach(function(item) {
+                cookie += item.split(';')[0] + "; "
+            });
+        }
+        test.ok(resp1.headers["www-authenticate"]);
+        var match = resp1.headers["www-authenticate"].match(/^Digest\s+(.*)/);
+        test.ok(match);
+        resp1.on("end", function () {
+            if (match) {
+                var authreq = parseDigestHeaderString(match[1]);
+                test.equal(authreq.realm, "NKServerTest");
+                test.ok(authreq.nonce);
+                test.equal(authreq.qop, "auth");
+                test.ok(authreq.opaque);
+
+                var auth = generateDigestAuth(authreq, "/test/secured", "test", "test");
+
+                var req2 = client.request("GET", "/test/secured", {"host": "127.0.0.1", "cookie": cookie, "Authorization": auth});
+                req2.on("response", function (resp2) {
+                    resp2.setEncoding("utf8");
+                    test.equal(resp2.statusCode, 200);
+                    test.equal(resp2.headers["content-type"], "text/plain");
+                    test.ok(resp2.headers["set-cookie"]);
+                    resp2.on("data", function (body) {
+                        test.ok(body);
+                        test.ok(body.indexOf("content") >= 0)
+                    });
+                    resp2.on("end", function () {
+                        var req3 = client.request("GET", "/test/logout", { "host": "127.0.0.1", "cookie": cookie });
+                        req3.on("response", function (resp3) {
+                            resp3.setEncoding("utf8");
+                            test.equal(resp3.statusCode, 200);
+                            test.equal(resp3.headers["content-type"], "text/plain");
+                            resp3.on("data", function (body) {
+                                test.ok(body);
+                                test.ok(body.indexOf("content") >= 0)
+                            });
+                            resp3.on("end", function () {
+                                var req4 = client.request("GET", "/test/secured", { "host": "127.0.0.1", "cookie": cookie });
+                                req4.on("response", function (resp4) {
+                                    resp4.setEncoding("utf8");
+                                    test.equal(resp4.statusCode, 401);
+                                    test.ok(resp4.headers["www-authenticate"]);
+                                    test.ok(resp4.headers["www-authenticate"].match(/^Digest\s+(.*)/));
+                                    resp4.on("end", function () {
+                                        test.done();
+                                    });
+                                });
+                                req4.end();
+                            });
+                        });
+                        req3.end();
+                    });
+                });
+                req2.end();
+            } else {
+                test.done();
+            }
+        });
+    });
+    req1.end();
+};
+
+module.exports["Test logout without auth (Digest)"] = function(test) {
+    var client = http.createClient(port, "127.0.0.1");
+    var req1 = client.request("GET", "/test/secured", { "host": "127.0.0.1" });
+    req1.on("response", function (resp1) {
+        resp1.setEncoding("utf8");
+        test.equal(resp1.statusCode, 401);
+        var cookie = "";
+        test.ok(resp1.headers["set-cookie"]);
+        if (resp1.headers["set-cookie"]) {
+            var cresp = resp1.headers["set-cookie"];
+            cresp.forEach(function(item) {
+                cookie += item.split(';')[0] + "; "
+            });
+        }
+        test.ok(resp1.headers["www-authenticate"]);
+        test.ok(resp1.headers["www-authenticate"].match(/^Digest\s+(.*)/));
+        resp1.on("end", function () {
+            var req2 = client.request("GET", "/test/logout", { "host": "127.0.0.1", "cookie": cookie });
+            req2.on("response", function (resp2) {
+                resp2.setEncoding("utf8");
+                test.equal(resp2.statusCode, 200);
+                test.equal(resp2.headers["content-type"], "text/plain");
+                resp2.on("data", function (body) {
+                    test.ok(body);
+                    test.ok(body.indexOf("content") >= 0)
+                });
+                resp2.on("end", function () {
+                    var req3 = client.request("GET", "/test/secured", { "host": "127.0.0.1", "cookie": cookie });
+                    req3.on("response", function (resp3) {
+                        resp3.setEncoding("utf8");
+                        test.equal(resp3.statusCode, 401);
+                        test.ok(resp3.headers["www-authenticate"]);
+                        test.ok(resp3.headers["www-authenticate"].match(/^Digest\s+(.*)/));
+                        resp3.on("end", function () {
+                            test.done();
+                        });
+                    });
+                    req3.end();
+                });
+            });
+            req2.end();
+        });
+    });
+    req1.end();
+};
 
 module.exports["Test shutdown server (Digest)"] = function(test) {
     test.ok(nserver);
