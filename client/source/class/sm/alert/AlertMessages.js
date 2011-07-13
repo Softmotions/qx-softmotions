7f573c407d4ba9eb054e6cbf11217e5d479f3039
adam <anton@adamansky.com>
2011-07-13 19:38:17 +0700

    sm.alert.AlertMessages fixes

--- client/source/class/sm/alert/AlertMessages.js
+++ client/source/class/sm/alert/AlertMessages.js
@@ -7,88 +7,104 @@
  * Алерт
  */
 qx.Class.define("sm.alert.AlertMessages", {
-      extend : qx.ui.window.Window,
+    extend : qx.ui.window.Window,
 
-      events :
-      {
-      },
+    events :
+    {
+    },
 
-      properties :
-      {
-      },
+    properties :
+    {
+    },
 
-      construct : function(caption) {
-          this.base(arguments, caption);
-          this.setLayout(new qx.ui.layout.VBox(10));
-          this.set({
-                modal         : true,
-                showMinimize  : false,
-                showMaximize  : false,
-                allowMaximize : false,
-                width : 390
-            });
+    construct : function(caption) {
+        this.base(arguments, caption);
+        this.setLayout(new qx.ui.layout.VBox(10));
+        this.set({
+            modal         : true,
+            showMinimize  : false,
+            showMaximize  : false,
+            allowMaximize : false,
+            width : 390
+        });
 
-          this.__messages = {};
-          this.__container = new qx.ui.container.Composite(new qx.ui.layout.VBox());
-          this.add(this.__container);
-          var cancel = new qx.ui.form.Button(this.tr("Close"));
-          this.add(cancel);
-          cancel.addListener("execute", function() {
-              this.close();
-          }, this);
-          this.addListenerOnce("resize", function() {
-              this.center();
-              cancel.focus();
-          }, this);
-      },
+        this.__messages = {};
+        this.__container = new qx.ui.container.Composite(new qx.ui.layout.VBox());
+        this.add(this.__container);
+        var cancel = new qx.ui.form.Button(this.tr("Close"));
+        this.add(cancel);
+        cancel.addListener("execute", function() {
+            this.close();
+        }, this);
+        this.addListenerOnce("resize", function() {
+            this.center();
+            cancel.focus();
+        }, this);
+    },
 
-      members :
-      {
+    members :
+    {
 
-          __container : null,
+        __container : null,
 
-          __messages : null,
+        __messages : null,
 
-          ensureOnTop : function() {
-              var root = qx.core.Init.getApplication().getRoot();
-              var maxWindowZIndex = this.getZIndex();
-              var windows = root.getWindows();
-              for (var i = 0; i < windows.length; i++) {
-                  if (windows[i] != this) {
-                      var zIndex = windows[i].getZIndex();
-                      maxWindowZIndex = Math.max(maxWindowZIndex, zIndex);
-                  }
-              }
-              var me = this;
-              me.setZIndex(maxWindowZIndex + 1);
-          },
+        ensureOnTop : function() {
+            var root = qx.core.Init.getApplication().getRoot();
+            var maxWindowZIndex = this.getZIndex();
+            var windows = root.getWindows();
+            for (var i = 0; i < windows.length; i++) {
+                if (windows[i] != this) {
+                    var zIndex = windows[i].getZIndex();
+                    maxWindowZIndex = Math.max(maxWindowZIndex, zIndex);
+                }
+            }
+            var me = this;
+            me.setZIndex(maxWindowZIndex + 1);
+        },
 
-          open : function() {
-              this.base(arguments);
-              this.ensureOnTop();
-          },
+        open : function() {
+            this.base(arguments);
+            this.ensureOnTop();
+        },
 
 
-          addMessages : function(caption, messages) {
-              if (!messages || messages.length == 0) {
-                  return;
-              }
-              var msgs = this.__messages[caption];
-              if (msgs == null) {
-                  msgs = new qx.ui.groupbox.GroupBox(caption);
-                  msgs.setLayout(new qx.ui.layout.VBox());
-                  this.__container.add(msgs);
-                  this.__messages[caption] = msgs;
-              }
-              for (var i = 0; i < messages.length; ++i) {
-                  var blb = new qx.ui.basic.Label(messages[i]).set({rich:true});
-                  msgs.add(blb);
-              }
-          }
-      },
+        close : function() {
+            this.base(arguments);
+            this.resetMessages();
+        },
 
-      destruct : function() {
-          this.__messages = null;
-          this._disposeObjects("__container");
-      }
-  });
+
+
+        addMessages : function(caption, messages) {
+            if (!messages || messages.length == 0) {
+                return;
+            }
+            var msgs = this.__messages[caption];
+            if (msgs == null) {
+                msgs = new qx.ui.groupbox.GroupBox(caption);
+                msgs.setLayout(new qx.ui.layout.VBox());
+                this.__container.add(msgs);
+                this.__messages[caption] = msgs;
+            }
+            if (!qx.lang.Type.isArray(messages)) {
+                messages = [messages.toString()];
+            }
+            for (var i = 0; i < messages.length; ++i) {
+                var blb = new qx.ui.basic.Label(messages[i]).set({rich:true});
+                msgs.add(blb);
+            }
+        },
+
+        resetMessages : function() {
+            this.__container.removeAll();
+            this._disposeMap("__messages");
+            this.__messages = {};
+        }
+    },
+
+    destruct : function() {
+        this.resetMessages();
+        this._disposeObjects("__container");
+    }
+});
