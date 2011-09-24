97ffc437e36bf5f9fa8fe705ccb9922419fc4621
adam <anton@adamansky.com>
2011-09-24 19:47:02 +0700

    mediahouse#63

---
+++ client/source/class/sm/ui/core/ExtendedCommand.js
@@ -0,0 +1,29 @@
+/**
+ * Extended command
+ * which uses {@link sm.bom.ExtendedShortcut}
+ * for event handling
+ */
+qx.Class.define("sm.ui.core.ExtendedCommand", {
+    extend  : qx.ui.core.Command,
+
+    /**
+     * @param shortcut {String} Shortcuts can be composed of optional modifier
+     *    keys Control, Alt, Shift, Meta and a non modifier key.
+     *    If no non modifier key is specified, the second paramater is evaluated.
+     *    The key must be separated by a <code>+</code> or <code>-</code> character.
+     *    Examples: Alt+F1, Control+C, Control+Alt+Delete
+     */
+    construct : function(shortcut, dontStop) {
+        this._shortcut = new sm.bom.ExtendedShortcut(shortcut, dontStop);
+        this._shortcut.addListener("execute", this.execute, this);
+    },
+
+    members :
+    {
+
+    },
+
+    destruct : function() {
+        //this._disposeObjects("__field_name");
+    }
+});
