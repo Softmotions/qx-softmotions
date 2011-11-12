e0753189b8c000e6c511bb2bbde142618d5a11c6
adam <anton@adamansky.com>
2011-11-12 22:27:19 +0700

    #76

--- client/source/class/sm/ui/cont/LazyStack.js
+++ client/source/class/sm/ui/cont/LazyStack.js
@@ -57,6 +57,13 @@ qx.Class.define("sm.ui.cont.LazyStack", {
         },
 
         /**
+         * Return array of widget ids
+         */
+        getWidgetIds : function() {
+            return Object.keys(this.__slots);
+        },
+
+        /**
          * Show widget with given id on top of the stack
          * @param id {String} widget id
          */
