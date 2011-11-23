2c35677062849ac02793060b370497f3cb2436ab
Alexey Sokolov <you@example.com>
2011-11-23 11:53:42 +0700

    Replace links in wikified HTML to aliases.

--- node/qxoo/class/sm/cms/wiki/WikiMarkupExecutor.js
+++ node/qxoo/class/sm/cms/wiki/WikiMarkupExecutor.js
@@ -12,8 +12,27 @@ qx.Class.define("sm.cms.wiki.WikiMarkupExecutor", {
          * Generate html code based on markup
          */
         __wiki : function(req, resp, ctx) {
-            resp.end(ctx["html"] ? ctx["html"] : "");
-            ctx({"terminated" : true});
+            var html = ctx["html"] || "";
+            var registry = sm.cms.page.AliasRegistry.getInstance();
+            var re = /href="(?:http:\/\/[\w.]+)?\/exp\/p([0-9a-f]{24})"/g;
+            function searchNext() {
+                var res = re.exec(html);
+                if (res) {
+                    // replace and continue
+                    registry.findAliasByPage(res[1], function(alias) {
+                        var before = html.slice(0, res.index);
+                        var after = html.slice(res.index + res[0].length);
+                        var replace = 'href="/exp/' + alias + '"';
+                        html = before + replace + after;
+                        re.lastIndex += replace.length - res[0].length;
+                        searchNext();
+                    });
+                } else {
+                    resp.end(html);
+                    ctx({"terminated" : true});
+                }
+            }
+            searchNext();
         }
     },
 
