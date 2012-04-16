ce9368e95e6c4832d1827f3addba712ae8222fb0
adam <anton@adamansky.com>
2012-04-16 21:53:40 +0700

    platform fixes

--- node/qxoo/class/sm/nsrv/tengines/JazzTemplateEngine.js
+++ node/qxoo/class/sm/nsrv/tengines/JazzTemplateEngine.js
@@ -133,7 +133,7 @@ qx.Class.define("sm.nsrv.tengines.JazzTemplateEngine", {
                 return;
             }
             ctx["_global_"] = window;
-            ctx["_env_"] = sm.app.Env.getDefault();
+            ctx["_env_"] = sm.app.Env.getDefault(true);
             ctx["_ctx_"] = ctx;
             ctx["_req_"] = req;
             ctx["_res_"] = res;
