72bf6a30ec9007cab7cbc29237a2ae824f736c9e
adam <anton@adamansky.com>
2012-08-05 16:03:38 +0700

    fixed run.js

--- node/node/utils/run.js
+++ node/node/utils/run.js
@@ -1,47 +1,47 @@
-var daemon = require("daemon");
-var fs = require("fs");
-var path = require("path");
-var fsutils = require("./fsutils");
-
-var args = process.argv;
-
-var cAction = null;
-var cAppRoot = null;
-var cVarRoot = null;
-var cScript = null;
-
-
 (function() {
-    var args = process.argv;
-    for (var i = 2; i < args.length; ++i) {
-        var a = args[i];
-        if (["start", "stop"].indexOf(a) != -1) {
-            cAction = a;
-        } else if ("--app" == a && args[i + 1] != null) {
-            cAppRoot = path.resolve(args[i + 1]);
-        } else if ("--var" == a && args[i + 1] != null) {
-            cVarRoot = path.resolve(args[i + 1]);
-        } else if ("--script" == a && args[i + 1] != null) {
-            cScript = path.resolve(args[i + 1]);
+
+    var daemon = require("daemon");
+    var fs = require("fs");
+    var path = require("path");
+    var fsutils = require("./fsutils");
+
+    var cAction = null;
+    var cAppRoot = null;
+    var cVarRoot = null;
+    var cScript = null;
+
+    (function() {
+        var args = process.argv;
+        for (var i = 2; i < args.length; ++i) {
+            var a = args[i];
+            if (["start", "stop"].indexOf(a) != -1) {
+                cAction = a;
+            } else if ("--app" == a && args[i + 1] != null) {
+                cAppRoot = path.resolve(args[i + 1]);
+            } else if ("--var" == a && args[i + 1] != null) {
+                cVarRoot = path.resolve(args[i + 1]);
+            } else if ("--script" == a && args[i + 1] != null) {
+                cScript = path.resolve(args[i + 1]);
+            }
         }
+    })();
+
+    if (cScript == null) {
+        console.log("Missing --script argument");
+        process.exit(1);
     }
-})();
 
-if (cScript == null) {
-    console.log("Missing --script argument");
-    process.exit(1);
-}
+    if (!cAction) {
+        return;
+    }
 
-if (cAppRoot == null) {
-    cAppRoot = __dirname;
-}
+    if (cAppRoot == null) {
+        cAppRoot = __dirname;
+    }
 
-if (cVarRoot == null) {
-    cVarRoot = __dirname;
-}
-
-
-if (cAction) {
+    if (cVarRoot == null) {
+        cVarRoot = __dirname;
+    }
 
     var config = {
         lockFile : path.join(cVarRoot, "daemon.pid"),
@@ -54,18 +54,13 @@ if (cAction) {
                 console.log("Service not running");
                 process.exit(0);
             }
-            console.log("Service stopped");
-            try {
-                process.kill(parseInt(fs.readFileSync(config.lockFile)));
-            } catch(e) {
-                console.log("No process killed");
-            } finally {
-                try {
-                    fs.unlinkSync(config.lockFile);
-                } catch(e) {
+            daemon.kill(config.lockFile, function(err) {
+                if (err) {
+                    console.error(err);
                 }
-            }
-            process.exit(0);
+                console.log("Service stopped");
+                process.exit(0);
+            });
             break;
 
         case "start":
@@ -75,7 +70,6 @@ if (cAction) {
                     try {
                         process.kill(parseInt(fs.readFileSync(config.lockFile)), 0);
                     } catch(e) {
-
                         if (e.code == "ESRCH") { //No such process
                             running = false;
                             try {
@@ -90,17 +84,16 @@ if (cAction) {
                     }
                 })();
             }
-            var fd = fs.openSync(config.logFile, 'w+');
             try {
-                var pid = daemon.start(fd);
-                daemon.lock(config.lockFile);
+                daemon.daemonize(config.logFile, config.lockFile);
             } catch (ex) {
                 console.log("Error starting daemon: " + ex);
                 process.exit(1);
             }
             break;
     }
-}
+
+})();
 
 //Starting
 require(cScript);
