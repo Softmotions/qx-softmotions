ad57df155aa51dedfbef3924834050d3914db9cd
Антон Адаманский <adamansky@gmail.com>
2014-04-28 23:39:41 +0700

    asm editor gui

---
+++ client/source/class/sm/ui/form/BaseSavePopupDlg.js
@@ -0,0 +1,25 @@
+/**
+ * BasePopupDlg with Save/Cancel buttons.
+ */
+qx.Class.define("sm.ui.form.BaseSavePopupDlg", {
+    extend : sm.ui.form.BasePopupDlg,
+
+    construct : function() {
+        this.base(arguments);
+        var vmgr = this._form.getValidationManager();
+        vmgr.setRequiredFieldMessage(this.tr("This field is required"));
+    },
+
+    members :
+    {
+        _configureFormButtons : function(form) {
+            var ok = new qx.ui.form.Button(this.tr("Save"));
+            ok.addListener("execute", this.save, this);
+            form.addButton(ok);
+
+            var cancel = new qx.ui.form.Button(this.tr("Cancel"));
+            cancel.addListener("execute", this.hide, this);
+            form.addButton(cancel);
+        }
+    }
+});
