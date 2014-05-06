e714c6a0004ef0549cfc83a5c1279142490388b1
Антон Адаманский <adamansky@gmail.com>
2014-05-06 16:46:31 +0700

    Added ExtendedVirtualTreeItem

---
+++ client/source/class/sm/ui/tree/ExtendedVirtualTreeItem.js
@@ -0,0 +1,26 @@
+/**
+ * Exteneded virrual tree item
+ */
+qx.Class.define("sm.ui.tree.ExtendedVirtualTreeItem", {
+    extend : qx.ui.tree.VirtualTreeItem,
+
+    statics : {
+    },
+
+    events : {
+    },
+
+    properties : {
+    },
+
+    construct : function() {
+        this.base(arguments);
+    },
+
+    members : {
+
+    },
+
+    destruct : function() {
+    }
+});
