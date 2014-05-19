7c2f02a105a375cc4e0b633c4394bde67c0a1034
Антон Адаманский <adamansky@gmail.com>
2014-05-19 22:35:44 +0700

    #1 media gui move folders

--- client/source/class/sm/ui/tree/ExtendedVirtualTree.js
+++ client/source/class/sm/ui/tree/ExtendedVirtualTree.js
@@ -2,11 +2,37 @@ qx.Class.define("sm.ui.tree.ExtendedVirtualTree", {
     extend : qx.ui.tree.VirtualTree,
 
     construct : function(model, labelPath, childProperty) {
-        this.base(arguments);
+        this.base(arguments, model, labelPath, childProperty);
     },
 
     members : {
 
+        findCachedNodeByPath : function(path, labelProperty) {
+            labelProperty = "get" + qx.lang.String.firstUp(labelProperty || "label");
+            if (typeof path === "string") {
+                path = path.split("/");
+            }
+            if (path.length === 0) {
+                return this.getModel();
+            }
+            var lt = this.getLookupTable();
+            for (var i = 0, l = lt.length; i < l; ++i) {
+                var j = path.length;
+                var item = lt.getItem(i);
+                var parent = item;
+                while (j > 0 && parent != null) {
+                    if (parent[labelProperty]() == path[--j]) {
+                        parent = this.getParent(parent);
+                        if (j === 0) {
+                            return item;
+                        }
+                    } else {
+                        break;
+                    }
+                }
+            }
+            return null;
+        }
     },
 
     destruct : function() {
